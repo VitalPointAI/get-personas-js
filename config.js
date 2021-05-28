33ad278dd26b9bc22694e1db4a6cbef453d92218
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-05-28 17:05:27 -0400

    try new url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // testnet / default
 let config = {
     APP_OWNER_ACCOUNT: 'vitalpointai.testnet',
-    CERAMIC_API_URL: 'https://ceramic-clay.3boxlabs.com',
+    CERAMIC_API_URL: 'https://gateway-clay.ceramic.network',
     networkId: 'testnet',
     nodeUrl: 'https://rpc.testnet.near.org',
     walletUrl: 'https://wallet.testnet.near.org',
