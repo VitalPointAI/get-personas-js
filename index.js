d9296087bcecdb59a72cf00d75af8fb9a6b2ddae
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-05-30 10:20:06 -0400

    added daoProfiles

--- index.js
+++ index.js
@@ -72,32 +72,43 @@ class Persona {
         const appClient = await this.getAppCeramic()
         const profile = this.getAlias(APP_OWNER_ACCOUNT, 'profile', contract)
         const donations = this.getAlias(APP_OWNER_ACCOUNT, 'donations', contract)
+        const daoProfile = this.getAlias(APP_OWNER_ACCOUNT, 'daoProfile', contract)
 
         const done = await Promise.all([
         profile,
-        donations
+        donations,
+        daoProfile
         ])
         
         let rootAliases = {
         profile: done[0],
-        donations: done[1]
+        donations: done[1],
+        daoProfile: done[2]
         }
-
+        
         const appIdx = new IDX({ ceramic: appClient, aliases: rootAliases})
-    
         return appIdx
     }
 
     async getPersona(accountId) {
         let contract = await this.initiateDidRegistryContract(accountId)
         let idx = await this.getAppIdx(contract)
-       
         let did = await this.getDID(accountId, contract)
-        
         let persona = await idx.get('profile', did)
-       
         if(persona){
             return persona
+        } else {
+            return false
+        }
+    }
+
+    async getDao(accountId){
+        let contract = await this.initiateDidRegistryContract(accountId)
+        let idx = await this.getAppIdx(contract)
+        let did = await this.getDID(accountId, contract)
+        let dao = await idx.get('daoProfile', did)
+        if(dao){
+            return dao
         } else {
             return false
         }
