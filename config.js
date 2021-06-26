544545886d742124873285978eee5d586f39e245
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-06-26 19:49:24 -0400

    fix https

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 let config = {
     APP_OWNER_ACCOUNT: 'vitalpointai.testnet',
    // CERAMIC_API_URL: 'https://ceramic-clay.3boxlabs.com',
-    CERAMIC_API_URL: 'http://ceramic-node.vitalpointai.com:7007',
+    CERAMIC_API_URL: 'https://ceramic-node.vitalpointai.com:7007',
    // CERAMIC_API_URL: 'https://gateway-clay.ceramic.network',
     networkId: 'testnet',
     nodeUrl: 'https://rpc.testnet.near.org',
