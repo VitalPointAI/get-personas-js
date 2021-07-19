62925ad66553789779202e05a1591c6182939b54
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-07-19 07:10:43 -0400

    changed ceramic node

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // testnet / default
 let config = {
     APP_OWNER_ACCOUNT: 'vitalpointai.testnet',
-   // CERAMIC_API_URL: 'https://ceramic-clay.3boxlabs.com',
+    CERAMIC_API_URL: 'https://ceramic-clay.3boxlabs.com',
    // CERAMIC_API_URL: 'https://ceramic-node.vitalpointai.com:7007',
-    CERAMIC_API_URL: 'https://gateway-clay.ceramic.network',
+   // CERAMIC_API_URL: 'https://gateway-clay.ceramic.network',
     networkId: 'testnet',
     nodeUrl: 'https://rpc.testnet.near.org',
     walletUrl: 'https://wallet.testnet.near.org',
