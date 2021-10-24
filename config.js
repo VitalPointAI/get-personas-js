0019ee2e91dc78d522782f8377f060fe1ad6ea93
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-10-24 19:02:26 -0400

    update packages

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 // // testnet / default
-// let config = {
-//     APP_OWNER_ACCOUNT: 'vitalpointai.testnet',
-//     CERAMIC_API_URL: 'https://ceramic-node.vitalpointai.com',
-//     networkId: 'testnet',
-//     nodeUrl: 'https://rpc.testnet.near.org',
-//     walletUrl: 'https://wallet.testnet.near.org',
-//     nameSuffix: '.testnet',
-//     contractName: 'testnet',
-//     didRegistryContractName: 'dids1.vitalpointai.testnet',
-// }
+let config = {
+    APP_OWNER_ACCOUNT: 'vitalpointai.testnet',
+    CERAMIC_API_URL: 'https://ceramic-node.vitalpointai.com',
+    networkId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    nameSuffix: '.testnet',
+    contractName: 'testnet',
+    didRegistryContractName: 'dids1.vitalpointai.testnet',
+}
 
-//if (process.env.ENV === 'prod') {
+if (process.env.ENV === 'prod') {
 let config = {
         APP_OWNER_ACCOUNT: 'aaron.near',
         CERAMIC_API_URL: 'https://ceramic-node.vitalpointai.com',
@@ -21,6 +21,6 @@ let config = {
         contractName: 'near',
         didRegistryContractName: 'did.near',
     }
-//}
+}
 
 export { config }
